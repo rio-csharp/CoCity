1b7dea97c39d04155f09678dd70ce890d52b4add
rio-csharp <26743502+rio-csharp@users.noreply.github.com>
2026-04-12 23:21:13 +0800

    feat: add initial .NET MAUI app scaffold

---
+++ CoCity/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CoCity
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
